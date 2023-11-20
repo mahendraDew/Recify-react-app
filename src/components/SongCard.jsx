ec3147952a85ce128d3f7e5591d1556bdcbdf92a
“MahendraDew” <mahendradewangan195@gmail.com>
2023-11-20 22:35:28 +0530

    added function to handle play/pause songs

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,16 +3,16 @@ import { useDispatch } from 'react-redux'
 import PlayPause from './PlayPause'
 import { playPause, setActiveSong } from '../redux/features/playerSlice'
 
-
-const handlePlayClick = (song, dispatch) => {
-
-}
-const handlePauseClick = (song, dispatch) => {
-
-}
-
-
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch()
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({song, data, i})),
+    dispatch(playPause(true));
+  }
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  }
 
   return (
     <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
@@ -24,21 +24,31 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
               : 'hidden'
           }`}
         >
-          <PlayPause isPlaying={isPlaying} activeSong={activeSong} song={song} handlePause={handlePauseClick} handlePlay={handlePlayClick}/>
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
-        <img alt='song_img' src={song.images?.coverart}/>
+        <img alt='song_img' src={song.images?.coverart} />
       </div>
       <div className='mt-4 flex flex-col'>
-          <p className='font-semibold text-lg text-white truncate'>
-            <Link to={`/songs/${song?.key}`}>
-              {song.title}
-            </Link>
-          </p>
-          <p className='text-sm truncate text-gray-300 mt-1'>
-            <Link to={song.artists ?  `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
-              {song.subtitle}
-            </Link>
-          </p>
+        <p className='font-semibold text-lg text-white truncate'>
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+        </p>
+        <p className='text-sm truncate text-gray-300 mt-1'>
+          <Link
+            to={
+              song.artists
+                ? `/artists/${song?.artists[0]?.adamid}`
+                : '/top-artists'
+            }
+          >
+            {song.subtitle}
+          </Link>
+        </p>
       </div>
     </div>
   )
