20115319405ef5e86bce2b9e98ca4b163db53852
“MahendraDew” <mahendradewangan195@gmail.com>
2023-11-20 22:18:11 +0530

    discover page - fetch country-top-song

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,40 @@
-const Discover = () => <div>Discover</div>;
+import { useDispatch, useSelector } from 'react-redux';
+import { Error, Loader, SongCard } from '../components'
+import { genres } from '../assets/constants'
 
-export default Discover;
+import { useGetTopChartsQuery } from '../redux/services/shazam'
+
+
+
+const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data, isFetching, error } = useGetTopChartsQuery();
+
+    console.log(data);
+
+    if(isFetching) return <Loader tittle="Loading songs..."/>
+
+    if(error) return <Error />
+
+    const genreTitle = "pop";
+    // console.log(genres);
+    return (
+        <div className='flex flex-col '>
+            <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
+                <h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
+                <select onChange={() => {}} value="" className="bg-black text-gray-300 text-sm rounded-lg outline-none sm:mt-0 mt-5">
+                    {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+                </select>
+            </div>
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {
+                    data.result.tracks.slice(0, 50)?.map((song, i) => <SongCard key={song.key} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} data={data}/>)
+                }
+
+            </div>
+        </div>
+    );
+}
+
+export default Discover
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import playerReducer from './features/playerSlice';
+import { shazamAPI } from './services/shazam';
 
 export const store = configureStore({
   reducer: {
+    [shazamAPI.reducerPath]: shazamAPI.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamAPI.middleware),
 });
